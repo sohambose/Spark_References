b96374c330b7fdd78f14b9f25312aaf3f33025a7
sohambose <sohamboseofficial@gmail.com>
2021-05-13 17:09:21 +0530

    Comments Added

--- src/main/scala/RDD.scala
+++ src/main/scala/RDD.scala
@@ -3,8 +3,13 @@ import org.apache.log4j._
 
 object RDD extends App {
 
+  //----For Setting Log Level-----
   Logger.getLogger("org").setLevel(Level.ERROR)
+
+  //----Create Spark Context-----
   val sc = new SparkContext("local[*]", "RatingsCounter")
+
+  //-----Use the Spark Context to get textfile
   val lines = sc.textFile("DataFiles/movieRatings.data")
   val ratings = lines.map(x => x.split("\t")(2))
   val results = ratings.countByValue()
